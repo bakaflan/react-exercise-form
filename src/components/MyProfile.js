4c672d84ffaa4f84dc38f10f7c9de45b4f88e5af
bakaflan <184444280@qq.com>
2020-08-12 18:17:12 +0800

    [ruiling] feat: complete myprofile.js

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,14 +1,73 @@
-import React, {Component} from 'react';
-import './myProfile.less';
+import React from 'react';
 
-class MyProfile extends Component {
-  render() {
-    return (
-      <form></form>
-    );
-  }
+class MyProfile extends React.Component {
+    state = {
+        name: '',
+        gendar: 'male',
+        description: '',
+        check: false
+    }
+
+    handleNameChange = event => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
+    handleGendarChange = event => {
+        this.setState({
+            gendar: event.target.value
+        })
+    }
+
+    handleDescriptionChange = event => {
+      this.setState({
+        description: event.target.value
+      })
+    }
+
+    handleCheckChange = event => {
+      this.setState({
+        check: event.target.checked
+      })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        console.log(this.state.name, this.state.gendar, this.state.description, this.state.check);
+    }
+
+    render() {
+        return <form className = 'form'>
+            <div>
+                <label htmlFor = 'name'>Name:
+                    <input type = 'text' id = 'name' value = {this.state.name} onChange = {this.handleNameChange}/>
+                </label>
+            </div>
+            <div>
+                <label > Gendar
+                    <select value = {this.state.gendar} onChange={this.handleGendarChange}>
+                        <option value="male">male</option>
+                        <option value="female">female</option>
+                    </select>
+                </label>
+            </div>
+            <div>
+                <label htmlFor = 'description'>Description:
+                    <input type = 'text' id = 'description' value = {this.state.description} onChange = {this.handleDescriptionChange}/>
+                </label>
+            </div>
+            <div>
+              <input type = 'checkbox' onChange = {this.handleCheckChange}></input>
+              <label>I have read the terms of conduct</label>
+            </div>
+            <div>
+                <button type = 'submit' onClick = {this.handleSubmit} disabled={!this.state.name||!this.state.description||!this.state.gendar}>
+                    submit
+                </button>
+            </div>
+        </form>
+    }
 }
 
 export default MyProfile;
-
-
